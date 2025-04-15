b73187e5c846c85387d8b92aa29bd1e1d1425d4b
Andrew <Andrew@DESKTOP-8425NTP>
2025-04-14 21:19:00 -0500

    Add project files.

---
+++ Wk1Ex1Redo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex1Redo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print message
+            Console.WriteLine("Welcome to ITSE1302");
+        }
+    }
+}
